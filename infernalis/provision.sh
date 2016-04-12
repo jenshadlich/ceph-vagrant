c6cb2eb2d726d5dcd1bf14331185c07fda8cbcfd
Jens Hadlich <jenshadlich@googlemail.com>
2016-04-12 08:28:22 +0200

    use 2 OSDs; basic installation of radosgw

--- infernalis/provision.sh
+++ infernalis/provision.sh
@@ -6,6 +6,7 @@ export DEBIAN_FRONTEND=noninteractive
 #export FS_TYPE=xfs
 export CEPH_SERVER_NODE=ceph-infernalis
 export CEPH_RELEASE=infernalis
+export CEPH_RGW_PORT=8888
 
 wget -q -O- 'https://download.ceph.com/keys/release.asc' | sudo apt-key add -
 
@@ -20,6 +21,7 @@ ceph-deploy pkg --install librados-dev ${CEPH_SERVER_NODE}
 ceph-deploy new ${CEPH_SERVER_NODE}
 echo "osd crush chooseleaf type = 0" >> ceph.conf
 echo "osd pool default size = 1" >> ceph.conf
+echo "osd journal size = 128" >> ceph.conf
 
 ceph-deploy install ${CEPH_SERVER_NODE}
 ceph-deploy mon create-initial
@@ -27,8 +29,14 @@ ceph-deploy mon create-initial
 mkdir /var/local/osd0
 chown ceph:ceph /var/local/osd0
 
+mkdir /var/local/osd1
+chown ceph:ceph /var/local/osd1
+
 ceph-deploy osd prepare ${CEPH_SERVER_NODE}:/var/local/osd0
 ceph-deploy osd activate ${CEPH_SERVER_NODE}:/var/local/osd0
+
+ceph-deploy osd prepare ${CEPH_SERVER_NODE}:/var/local/osd1
+ceph-deploy osd activate ${CEPH_SERVER_NODE}:/var/local/osd1
 
 #ceph-deploy disk --fs-type ${FS_TYPE} zap ${CEPH_SERVER_NODE}:${DATA_DEVICE}
 #ceph-deploy disk --fs-type ${FS_TYPE} zap ${CEPH_SERVER_NODE}:${JOURNAL_DEVICE}
@@ -37,7 +45,33 @@ ceph-deploy osd activate ${CEPH_SERVER_NODE}:/var/local/osd0
 # version
 ceph -v
 
+echo "Wait 5 seconds ..."
+sleep 5
+
 # health
 ceph -s
 
-echo "Done."
+echo "Done CEPH."
+
+ceph-deploy install --rgw ${CEPH_SERVER_NODE}
+ceph-deploy rgw create ${CEPH_SERVER_NODE}
+
+echo "" >> ceph.conf
+echo "[client]" >> ceph.conf
+echo "rgw frontends = civetweb port=${CEPH_RGW_PORT}" >> ceph.conf
+
+echo "" >> ceph.conf
+echo "[client.rgw.${CEPH_SERVER_NODE}]" >> ceph.conf
+echo "host = ${CEPH_SERVER_NODE}" >> ceph.conf
+echo "log file = /var/log/radosgw/client.rgw.${CEPH_SERVER_NODE}.log" >> ceph.conf
+
+ceph-deploy --overwrite-conf config push ${CEPH_SERVER_NODE}
+service radosgw restart id="rgw.${CEPH_SERVER_NODE}"
+
+echo "Wait 5 seconds ..."
+sleep 5
+
+# check ListAllMyBucketsResult
+curl http://${CEPH_SERVER_NODE}:${CEPH_RGW_PORT}
+
+echo "Done CEPH OBJECT GATEWAY."
