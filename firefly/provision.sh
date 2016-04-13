a606a9eeeefcc3d2fec9e52a6e949428109650c5
Jens Hadlich <jenshadlich@googlemail.com>
2016-04-13 16:22:15 +0200

    update

--- firefly/provision.sh
+++ firefly/provision.sh
@@ -6,13 +6,14 @@ export DEBIAN_FRONTEND=noninteractive
 #export FS_TYPE=xfs
 export CEPH_SERVER_NODE=$(hostname -s)
 export CEPH_RELEASE=firefly
+export CEPH_RGW_PORT=8888
 
 wget -q -O- 'https://download.ceph.com/keys/release.asc' | sudo apt-key add -
 
 echo deb http://download.ceph.com/debian-${CEPH_RELEASE}/ $(lsb_release -sc) main | sudo tee /etc/apt/sources.list.d/ceph.list
 
-sudo apt-get update > /dev/null
-sudo apt-get install -q -y ceph-deploy
+apt-get update -q > /dev/null
+apt-get install -q -y ceph-deploy
 
 ceph-deploy install --release ${CEPH_RELEASE} ${CEPH_SERVER_NODE}
 ceph-deploy pkg --install librados-dev ${CEPH_SERVER_NODE}
@@ -20,23 +21,33 @@ ceph-deploy pkg --install librados-dev ${CEPH_SERVER_NODE}
 ceph-deploy new ${CEPH_SERVER_NODE}
 echo "osd crush chooseleaf type = 0" >> ceph.conf
 echo "osd pool default size = 1" >> ceph.conf
+echo "osd journal size = 128" >> ceph.conf
+echo "#debug ms = 1" >> ceph.conf
+echo "#debug rgw = 20" >> ceph.conf
 
 ceph-deploy install ${CEPH_SERVER_NODE}
 ceph-deploy mon create-initial ${CEPH_SERVER_NODE}
 
 mkdir /var/local/osd0
+mkdir /var/local/osd1
 
 ceph-deploy osd prepare ${CEPH_SERVER_NODE}:/var/local/osd0
 ceph-deploy osd activate ${CEPH_SERVER_NODE}:/var/local/osd0
+
+ceph-deploy osd prepare ${CEPH_SERVER_NODE}:/var/local/osd1
+ceph-deploy osd activate ${CEPH_SERVER_NODE}:/var/local/osd1
 
 #ceph-deploy disk --fs-type ${FS_TYPE} zap ${CEPH_SERVER_NODE}:${DATA_DEVICE}
 #ceph-deploy disk --fs-type ${FS_TYPE} zap ${CEPH_SERVER_NODE}:${JOURNAL_DEVICE}
 #ceph-deploy osd --fs-type ${FS_TYPE} create ${CEPH_SERVER_NODE}:${DATA_DEVICE}:${JOURNAL_DEVICE}
 
 # version
-sudo ceph -v
+ceph -v
+
+echo "Wait 5 seconds ..."
+sleep 5
 
 # health
-sudo ceph -s
+ceph -s
 
-echo "Done."
+echo "Done CEPH."
