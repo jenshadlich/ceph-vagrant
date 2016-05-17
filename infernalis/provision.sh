286b3f6fda5226e8e40008bda0bda228728ff0c9
Jens Hadlich <jenshadlich@googlemail.com>
2016-05-17 14:32:43 +0200

    revert previous changes; make sure correct version is installed

--- infernalis/provision.sh
+++ infernalis/provision.sh
@@ -17,16 +17,12 @@ ceph-deploy install --release ${CEPH_RELEASE} ${CEPH_SERVER_NODE}
 ceph-deploy pkg --install librados-dev ${CEPH_SERVER_NODE}
 
 ceph-deploy new ${CEPH_SERVER_NODE}
-echo "osd max object name len = 256" >> ceph.conf
-echo "osd max object namespace len = 64" >> ceph.conf
-echo "osd check max object name len on startup = false" >> ceph.conf
 echo "osd crush chooseleaf type = 0" >> ceph.conf
 echo "osd pool default size = 1" >> ceph.conf
 echo "osd journal size = 128" >> ceph.conf
 echo "#debug ms = 1" >> ceph.conf
 echo "#debug rgw = 20" >> ceph.conf
 
-ceph-deploy install ${CEPH_SERVER_NODE}
 ceph-deploy mon create-initial
 
 mkdir /var/local/osd0
@@ -54,7 +50,7 @@ echo "##############"
 echo "# Done CEPH. #"
 echo "##############"
 
-ceph-deploy install --rgw ${CEPH_SERVER_NODE}
+ceph-deploy install --release ${CEPH_RELEASE} --rgw ${CEPH_SERVER_NODE}
 ceph-deploy rgw create ${CEPH_SERVER_NODE}
 
 echo "" >> ceph.conf
